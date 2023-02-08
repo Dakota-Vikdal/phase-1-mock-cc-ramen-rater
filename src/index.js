b7adc4f5fb99d2e91096503942d8de5c6eb3b5e5
Dakota Vikdal <dakota.james19@hotmail.com>
2023-02-08 11:27:26 -0500

    ramen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-// write your code here
 function getRamen() {
     fetch('http://localhost:3000/ramens')
-    .then(res => res.json())
+    .then(response => response.json())
     .then(data => renderRamen(data))
 }
 getRamen();
@@ -10,41 +9,42 @@ getRamen();
 const ramenMenu = document.querySelector("#ramen-menu")
 
 //Ramen details
-const ramenImg = document.querySelector("#ramen-detail > img")
+const ramenImage = document.querySelector("#ramen-detail > img")
 const ramenName = document.querySelector("#ramen-detail > h2")
-const ramenRestaurant = document.querySelector("#ramen-detail > h3")
-const ramenRatings = document.querySelector("#rating-display")
+const ramenRes = document.querySelector("#ramen-detail > h3")
+const ramenRating = document.querySelector("#rating-display")
 const ramenComment = document.querySelector("#comment-display")
+
 
 
 function renderRamen(ramens) {
     ramens.forEach(ramen => {
-        const ramenImage = document.createElement('img');
-        ramenImage.src = ramen.image
-        ramenMenu.append(ramenImage);
-        ramenImage.addEventListener('click', (e) => {
-            e.preventDefault();
-            ramenImg.src = ramen.image
+        const img = document.createElement('img');
+        img.src = ramen.image;
+        ramenMenu.append(img);
+        img.addEventListener('click', () => { 
+            ramenImage.src = ramen.image
             ramenName.textContent = ramen.name
-            ramenRestaurant.textContent = ramen.restaurant
-            ramenRatings.textContent = ramen.rating
+            ramenRes.textContent = ramen.restaurant
+            ramenRating.textContent = ramen.rating
             ramenComment.textContent = ramen.comment
         })
     })
 }
 
+
 function newRamen() {
-    const ramenForm = document.querySelector('#new-ramen')
-    ramenForm.addEventListener('submit', (e) => {
+    const newRamenForm = document.querySelector("#new-ramen")
+    newRamenForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        let myObj = {
-            name: e.target.name.value,
-            restaurant: e.target.restaurant.value,
-            image: e.target.image.value,
-            rating: parseInt(e.target.rating.value),
-            comment: e.target['new-comment'].value
-        }
-        renderRamen([myObj])
+            let newObject = {
+                name: e.target.name.value,
+                restaurant: e.target.restaurant.value,
+                image: e.target.image.value,
+                rating: parseInt(e.target.rating.value),
+                comment: e.target['new-comment'].value,
+            }
+        console.log(newObject)
     })
 }
-newRamen()
+newRamen();
